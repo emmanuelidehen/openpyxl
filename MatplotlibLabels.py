84eb32197fbc8d75aaa55bde1ed72cf287aa041b
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2020-09-11 17:44:57 -0500

    Add files via upload

---
+++ MatplotlibLabels.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 17:35:04 2020
+
+@author: emmanuelidehen
+"""
+import numpy as np 
+import matplotlib.pyplot as plt
+
+x = np.array ([1,2,3,4,5])
+y = np.array ([6,7,8,9,10])
+
+plt.xlabel('population')
+plt.ylabel('temperature')
+plt.title('population distribution')
+
+
+plot = plt.scatter(x,y)
+plt.show(plot)
+
